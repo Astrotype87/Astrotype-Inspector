1cc550a8af52aa8f68f470892191f798df5e6959
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-05-31 19:39:23 +0800

    Create new MinValueAttribute

---
+++ Attributes/Value/MinValueAttribute.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace AstrotypeInspector
+{
+    [Conditional(Symbols.UNITY_EDITOR), Conditional(Symbols.INCLUDE_IN_BUILD)]
+    [AttributeUsage(AttributeTargets.Field)]
+    public class MinValueAttribute : PropertyAttribute
+    {
+        public readonly float Min;
+        
+        public MinValueAttribute(float min)
+        {
+            Min = min;
+        }
+    }
+}
+
+#if UNITY_EDITOR
+namespace AstrotypeInspector.Editor
+{
+    using UnityEngine;
+    using UnityEditor;
+    using UnityEngine.UIElements;
+    using UnityEditor.UIElements;
+    
+    [CustomPropertyDrawer(typeof(MinValueAttribute))]
+    public class MinValueDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+            
+            var attribute = this.attribute as MinValueAttribute;
+            
+            ApplyMinValue(property, attribute.Min);
+        }
+        
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            // Create property field
+            var propertyField = new PropertyField(property);
+            propertyField.schedule.Execute(() =>
+            {
+                // Unwrap property field wrapper
+                propertyField.UnwrapElement(out var parent);
+                
+                // Get the original property field
+                var originalField = parent.Q<PropertyField>();
+                originalField.RegisterValueChangeCallback(evt =>
+                {
+                    var attribute = this.attribute as MinValueAttribute;
+                    ApplyMinValue(property, attribute.Min);
+                });
+            });
+            
+            return propertyField;
+        }
+        
+        
+        private void ApplyMinValue(SerializedProperty property, float min)
+        {
+            if (property.propertyType == SerializedPropertyType.Integer)
+            {
+                property.intValue = Mathf.Max(property.intValue, (int)min);
+            }
+            if (property.propertyType == SerializedPropertyType.Float)
+            {
+                property.floatValue = Mathf.Max(property.floatValue, min);
+            }
+        }
+    }
+}
+#endif
