1f44494d5f3c5f41a2ff2e028bcb229c0b69c325
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-06-01 08:36:49 +0800

    Min value attribute UITK handling of UI and serialized property

--- Attributes/Value/MinValueAttribute.cs
+++ Attributes/Value/MinValueAttribute.cs
@@ -28,6 +28,8 @@ namespace AstrotypeInspector.Editor
     [CustomPropertyDrawer(typeof(MinValueAttribute))]
     public class MinValueDrawer : PropertyDrawer
     {
+        private bool isFocused;
+        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginChangeCheck();
@@ -38,13 +40,68 @@ namespace AstrotypeInspector.Editor
                 
                 if (property.propertyType == SerializedPropertyType.Float)
                 {
-                    property.floatValue = Mathf.Max(property.floatValue, attribute.Min);
+                    property.floatValue = Mathf.Max(attribute.Min, property.floatValue);
                 }
                 else if (property.propertyType == SerializedPropertyType.Integer)
                 {
-                    property.intValue = Mathf.Max(property.intValue, (int)attribute.Min);
+                    property.intValue = Mathf.Max((int)attribute.Min, property.intValue);
+                }
+                else
+                {
+                    EditorGUI.LabelField(position, label.text, "Other types not implemented.");
                 }
             }
+        }
+        
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var attribute = this.attribute as MinValueAttribute;
+            
+            // Create property field
+            var propertyField = new PropertyField(property);
+            propertyField.schedule.Execute(() =>
+            {
+                // Unwrap property field wrapper
+                propertyField.UnwrapElement(out var parent);
+                
+                if (property.propertyType == SerializedPropertyType.Float)
+                {
+                    if (property.type == "float")
+                    {
+                        var field = parent.Q<FloatField>();
+                        RegisterFocusBinding(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                Debug.Log($"InputEvent + isFocused");
+                                property.floatValue = Mathf.Max(attribute.Min, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                            }
+                        });
+                    }
+                }
+            });
+            
+            return propertyField;
+        }
+        
+        
+        private void RegisterFocusBinding(BindableElement element, SerializedProperty property)
+        {
+            element.RegisterCallback<FocusInEvent>(_ =>
+            {
+                Debug.Log($"FocusInEvent");
+                isFocused = true;
+                element.Unbind();
+            });
+            element.RegisterCallback<FocusOutEvent>(_ =>
+            {
+                Debug.Log($"FocusOutEvent");
+                isFocused = false;
+                element.Bind(property.serializedObject);
+            });
         }
     }
 }
