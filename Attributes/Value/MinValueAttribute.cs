d765c2e7c04a82c3b68f7db41818bdd91932758d
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-06-02 13:33:03 +0800

    Added vector types for CreatePropertyGUI

--- Attributes/Value/MinValueAttribute.cs
+++ Attributes/Value/MinValueAttribute.cs
@@ -158,7 +158,186 @@ namespace AstrotypeInspector.Editor
                             }
                         });
                     }
+                    else if (property.type == "double")
+                    {
+                        var field = parent.Q<DoubleField>();
+                        HandleFocusAndLabelDragging(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                property.doubleValue = Math.Max(attribute.MinX, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                                if (isDragging) field.value = property.doubleValue;
+                            }
+                        });
+                    }
                 }
+                else if (property.propertyType == SerializedPropertyType.Integer)
+                {
+                    if (property.type == "int")
+                    {
+                        var field = parent.Q<IntegerField>();
+                        HandleFocusAndLabelDragging(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                property.intValue = Mathf.Max((int)attribute.MinX, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                                if (isDragging) field.value = property.intValue;
+                            }
+                        });
+                    }
+                    else if (property.type == "long")
+                    {
+                        var field = parent.Q<LongField>();
+                        HandleFocusAndLabelDragging(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                property.longValue = Math.Max((long)attribute.MinX, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                                if (isDragging) field.value = property.longValue;
+                            }
+                        });
+                    }
+                    else if (property.type == "uint")
+                    {
+                        var field = parent.Q<UnsignedIntegerField>();
+                        HandleFocusAndLabelDragging(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                property.uintValue = Math.Max((uint)attribute.MinX, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                                if (isDragging) field.value = property.uintValue;
+                            }
+                        });
+                    }
+                    else if (property.type == "ulong")
+                    {
+                        var field = parent.Q<UnsignedLongField>();
+                        HandleFocusAndLabelDragging(field, property);
+                        field.RegisterCallback<InputEvent>(_ =>
+                        {
+                            if (isFocused)
+                            {
+                                property.ulongValue = Math.Max((ulong)attribute.MinX, field.value);
+                                property.serializedObject.ApplyModifiedProperties();
+                                property.serializedObject.Update();
+                                if (isDragging) field.value = property.ulongValue;
+                            }
+                        });
+                    }
+                }
+                else if (property.propertyType == SerializedPropertyType.Vector2)
+                {
+                    var field = parent.Q<Vector2Field>();
+                    HandleFocusAndLabelDragging(field, property);
+                    field.RegisterCallback<InputEvent>(_ =>
+                    {
+                        if (isFocused)
+                        {
+                            Vector2 value = property.vector2Value;
+                            property.vector2Value = new(
+                                Mathf.Max(attribute.MinX, value.x),
+                                Mathf.Max(attribute.MinY, value.y));
+                            
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector2Value;
+                        }
+                    });
+                }
+                else if (property.propertyType == SerializedPropertyType.Vector2Int)
+                {
+                    var field = parent.Q<Vector2IntField>();
+                    HandleFocusAndLabelDragging(field, property);
+                    field.RegisterCallback<InputEvent>(_ =>
+                    {
+                        if (isFocused)
+                        {
+                            Vector2Int value = property.vector2IntValue;
+                            property.vector2IntValue = new(
+                                Mathf.Max((int)attribute.MinX, value.x),
+                                Mathf.Max((int)attribute.MinY, value.y));
+                            
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector2IntValue;
+                        }
+                    });
+                }
+                else if (property.propertyType == SerializedPropertyType.Vector3)
+                {
+                    var field = parent.Q<Vector3Field>();
+                    HandleFocusAndLabelDragging(field, property);
+                    field.RegisterCallback<InputEvent>(_ =>
+                    {
+                        if (isFocused)
+                        {
+                            Vector3 value = property.vector3Value;
+                            property.vector3Value = new(
+                                Mathf.Max(attribute.MinX, value.x),
+                                Mathf.Max(attribute.MinY, value.y),
+                                Mathf.Max(attribute.MinZ, value.z));
+                            
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector3Value;
+                        }
+                    });
+                }
+                else if (property.propertyType == SerializedPropertyType.Vector3Int)
+                {
+                    var field = parent.Q<Vector3IntField>();
+                    HandleFocusAndLabelDragging(field, property);
+                    field.RegisterCallback<InputEvent>(_ =>
+                    {
+                        if (isFocused)
+                        {
+                            Vector3Int value = property.vector3IntValue;
+                            property.vector3IntValue = new(
+                                Mathf.Max((int)attribute.MinX, value.x),
+                                Mathf.Max((int)attribute.MinY, value.y),
+                                Mathf.Max((int)attribute.MinZ, value.z));
+                            
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector3IntValue;
+                        }
+                    });
+                }
+                else if (property.propertyType == SerializedPropertyType.Vector4)
+                {
+                    var field = parent.Q<Vector4Field>();
+                    HandleFocusAndLabelDragging(field, property);
+                    field.RegisterCallback<InputEvent>(_ =>
+                    {
+                        if (isFocused)
+                        {
+                            Vector4 value = property.vector4Value;
+                            property.vector4Value = new(
+                                Mathf.Max(attribute.MinX, value.x),
+                                Mathf.Max(attribute.MinY, value.y),
+                                Mathf.Max(attribute.MinZ, value.z),
+                                Mathf.Max(attribute.MinW, value.w));
+                            
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector4Value;
+                        }
+                    });
+                }
+                
+                
             });
             
             return propertyField;
