8c4a0fcc334821f48fdbcf84aed7eb8574522cc8
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-06-03 10:56:40 +0800

    Flip isFocused condition

--- Attributes/Value/MinValueAttribute.cs
+++ Attributes/Value/MinValueAttribute.cs
@@ -151,18 +151,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            Debug.Log($"[{property.name}] InputEvent");
-                            if (isFocused)
-                            {
-                                property.floatValue = Mathf.Max(attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging)
-                                {
-                                    Debug.Log($"UI field updated");
-                                    field.value = property.floatValue;
-                                }
-                            }
+                            if (!isFocused) return;
+                            property.floatValue = Mathf.Max(attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.floatValue;
                         });
                     }
                     else if (property.type == "double")
@@ -171,13 +164,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            if (isFocused)
-                            {
-                                property.doubleValue = Math.Max(attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging) field.value = property.doubleValue;
-                            }
+                            if (!isFocused) return;
+                            property.doubleValue = Math.Max(attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.doubleValue;
                         });
                     }
                 }
@@ -189,13 +180,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            if (isFocused)
-                            {
-                                property.intValue = Mathf.Max((int)attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging) field.value = property.intValue;
-                            }
+                            if (!isFocused) return;
+                            property.intValue = Mathf.Max((int)attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.intValue;
                         });
                     }
                     else if (property.type == "long")
@@ -204,13 +193,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            if (isFocused)
-                            {
-                                property.longValue = Math.Max((long)attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging) field.value = property.longValue;
-                            }
+                            if (!isFocused) return;
+                            property.longValue = Math.Max((long)attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.longValue;
                         });
                     }
                     else if (property.type == "uint")
@@ -219,13 +206,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            if (isFocused)
-                            {
-                                property.uintValue = Math.Max((uint)attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging) field.value = property.uintValue;
-                            }
+                            if (!isFocused) return;
+                            property.uintValue = Math.Max((uint)attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.uintValue;
                         });
                     }
                     else if (property.type == "ulong")
@@ -234,13 +219,11 @@ namespace AstrotypeInspector.Editor
                         HandleFocusAndDragging(field, property.serializedObject);
                         field.RegisterCallback<InputEvent>(_ =>
                         {
-                            if (isFocused)
-                            {
-                                property.ulongValue = Math.Max((ulong)attribute.MinX, field.value);
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                if (isDragging) field.value = property.ulongValue;
-                            }
+                            if (!isFocused) return;
+                            property.ulongValue = Math.Max((ulong)attribute.MinX, field.value);
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.ulongValue;
                         });
                     }
                 }
@@ -248,28 +231,18 @@ namespace AstrotypeInspector.Editor
                 {
                     var field = parent.Q<Vector3Field>();
                     HandleFocusAndDragging(field, property.serializedObject);
-                    
-                    var floatFields = parent.Query<FloatField>().ToList();
-                    foreach (var floatField in floatFields)
+                    foreach (var floatField in parent.Query<FloatField>().ToList())
                     {
                         floatField.RegisterCallback<ChangeEvent<float>>(_ =>
                         {
-                            Debug.Log($"[{field.name}] InputEvent");
-                            if (isFocused)
-                            {
-                                property.vector3Value = new(
-                                    Mathf.Max(attribute.MinX, field.value.x),
-                                    Mathf.Max(attribute.MinY, field.value.y),
-                                    Mathf.Max(attribute.MinZ, field.value.z));
-                                property.serializedObject.ApplyModifiedProperties();
-                                property.serializedObject.Update();
-                                
-                                if (isDragging)
-                                {
-                                    Debug.Log($"UI field updated");
-                                    field.value = property.vector3Value;
-                                }
-                            }
+                            if (!isFocused) return;
+                            property.vector3Value = new(
+                                Mathf.Max(attribute.MinX, field.value.x),
+                                Mathf.Max(attribute.MinY, field.value.y),
+                                Mathf.Max(attribute.MinZ, field.value.z));
+                            property.serializedObject.ApplyModifiedProperties();
+                            property.serializedObject.Update();
+                            if (isDragging) field.value = property.vector3Value;
                         });
                     }
                 }
@@ -284,13 +257,11 @@ namespace AstrotypeInspector.Editor
             // Detect property field focus
             element.RegisterCallback<FocusInEvent>(_ =>
             {
-                Debug.Log($"{element.name} => FocusInEvent --- isFocused = true, Unbind()");
                 isFocused = true;
                 element.Unbind();
             });
             element.RegisterCallback<FocusOutEvent>(_ =>
             {
-                Debug.Log($"{element.name} => FocusOutEvent --- isFocused = false, Bind()");
                 isFocused = false;
                 element.Bind(serializedObject);
             });
@@ -299,16 +270,8 @@ namespace AstrotypeInspector.Editor
             var labels = element.Query<Label>(className: "unity-base-text-field__label").ToList();
             foreach (var label in labels)
             {
-                label.RegisterCallback<PointerCaptureEvent>(_ =>
-                {
-                    Debug.Log($"{element.name} => PointerCaptureEvent --- isDragging = true");
-                    isDragging = true;
-                });
-                label.RegisterCallback<PointerCaptureOutEvent>(_ =>
-                {
-                    Debug.Log($"{element.name} => PointerCaptureOutEvent --- isDragging = false");
-                    isDragging = false;
-                });
+                label.RegisterCallback<PointerCaptureEvent>(_ => isDragging = true);
+                label.RegisterCallback<PointerCaptureOutEvent>(_ => isDragging = false);
             }
         }
         
