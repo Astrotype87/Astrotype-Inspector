ea7ded57f4d082dee8feee051691aa4546eaede9
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-06-06 19:22:39 +0800

    Implement InlineEditor in UIToolkit

--- Attributes/Preview/InlineEditorAttribute.cs
+++ Attributes/Preview/InlineEditorAttribute.cs
@@ -27,7 +27,15 @@ namespace AstrotypeInspector.Editor
     {
         private const string InvalidTypeMessage = "Use InlineEditor with object reference types.";
         
-        private Editor editor = null;
+        private Editor cachedEditor;
+        
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.ObjectReference)
+                return EditorGUIUtility.singleLineHeight;
+            
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
         
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
@@ -54,10 +62,10 @@ namespace AstrotypeInspector.Editor
                 return;
             
             // Create cached editor
-            if (editor == null || editor.target != property.objectReferenceValue)
-                Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+            if (cachedEditor == null || cachedEditor.target != property.objectReferenceValue)
+                Editor.CreateCachedEditor(property.objectReferenceValue, null, ref cachedEditor);
             
-            if (editor == null)
+            if (cachedEditor == null)
                 return;
             
             // Create box style
@@ -69,19 +77,102 @@ namespace AstrotypeInspector.Editor
             EditorGUILayout.BeginVertical(boxStyle);
             EditorGUI.indentLevel++;
             
-            editor.OnInspectorGUI();
+            cachedEditor.OnInspectorGUI();
             
             EditorGUI.indentLevel--;
             EditorGUILayout.EndVertical();
         }
         
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
             if (property.propertyType != SerializedPropertyType.ObjectReference)
-                return EditorGUIUtility.singleLineHeight;
+            {
+                var label = new Label(InvalidTypeMessage);
+                label.AddToClassList("unity-base-field__label");
+                label.style.marginLeft = 3;
+                label.style.marginBottom = 1;
+                label.style.marginTop = 1;
+                label.style.marginRight = 0;
+                return label;
+            }
             
-            return EditorGUI.GetPropertyHeight(property, label, true);
+            // Create property field
+            var propertyField = new PropertyField(property);
+            propertyField.schedule.Execute(() =>
+            {
+                // Unwrap property field wrapper
+                propertyField.UnwrapElement(out var parent);
+                
+                // Create foldout
+                var foldout = new Foldout();
+                foldout.name = "inline-editor-foldout";
+                foldout.value = property.isExpanded;
+                parent.Add(foldout);
+                
+                // Create editor container
+                var editorContainer = new VisualElement();
+                editorContainer.name =  "inline-editor-container";
+                foldout.Add(editorContainer);
+                
+                // Local variable for cached editor
+                Editor cachedEditor = null;
+                
+                // When foldout is expanded or collapsed
+                foldout.RegisterValueChangedCallback(e =>
+                {
+                    // Update property isExpanded state
+                    property.isExpanded = foldout.value;
+                    
+                    // HOW DO YOU KNOW IF EDITOR IS NOT CREATED YET? editorContainer is empty
+                    // IS THIS A RELIABLE BASIS? yes, because editorContainer is the proof that it contains the nested editor, and it is displayed in UI.
+                    // When foldout is opened and editorContainer is empty, create nested editor
+                    if (foldout.value && editorContainer.childCount == 0)
+                    {
+                        // If object reference is null, hide foldout and abort
+                        Object objectReference = property.objectReferenceValue;
+                        foldout.style.display = objectReference == null ? DisplayStyle.None : DisplayStyle.Flex;
+                        if (objectReference == null)
+                            return;
+                        
+                        // Create cached editor, abort if null
+                        Editor.CreateCachedEditor(objectReference, null, ref cachedEditor);
+                        if (cachedEditor == null)
+                            return;
+                        
+                        // Create inspector GUI and add to editor container
+                        var inspector = cachedEditor.CreateInspectorGUI();
+                        editorContainer.Add(inspector);
+                    }
+                });
+                
+                // Refresh nested editor if object field value is changed
+                propertyField.RegisterValueChangeCallback(e =>
+                {
+                    // Clear container and destroy cached editor
+                    editorContainer.Clear();
+                    Object.DestroyImmediate(cachedEditor);
+                    cachedEditor = null;
+                    
+                    // If object reference is null, hide foldout and abort
+                    Object objectReference = property.objectReferenceValue;
+                    foldout.style.display = objectReference == null ? DisplayStyle.None : DisplayStyle.Flex;
+                    if (objectReference == null)
+                        return;
+                    
+                    // Create cached editor, abort if null
+                    Editor.CreateCachedEditor(objectReference, null, ref cachedEditor);
+                    if (cachedEditor == null)
+                        return;
+                    
+                    // Create inspector GUI and add to editor container
+                    var inspector = cachedEditor.CreateInspectorGUI();
+                    editorContainer.Add(inspector);
+                });
+            });
+            
+            return propertyField;
         }
+        
     }
 }
 #endif
