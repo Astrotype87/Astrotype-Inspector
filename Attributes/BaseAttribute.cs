b89efc5de5006d92e6fc3d5d072648a6e429a2be
Astrotype <tolentinodavelaurence2048@gmail.com>
2026-06-14 20:14:02 +0800

    Move Symbols static class to BaseAttribute.cs

--- Attributes/BaseAttribute.cs
+++ Attributes/BaseAttribute.cs
@@ -2,24 +2,6 @@ using UnityEngine;
 
 namespace AstrotypeInspector
 {
-    /// <summary> Global priority of inspector attributes. Lower values are applied last. </summary>
-    public static class AttributePriority
-    {
-        public const int PostPropertyAttribute = -100;
-        public const int DecorativeAttribute = -200;
-        public const int WrapperAttribute = -200;
-        public const int GroupAttribute = -200;
-        
-        // DIFFERENT TYPES OF ATTRIBUTES : based on order and priority
-        // ----------------------------------------------------------------------
-        // UNITY PROPERTY ATTRIBUTES (Priority of 0)    - Range, Multiline, etc...
-        // POST PROPERTY ATTRIBUTES (Priority of -100)  - Slider
-        // DECORATIVE ATTRIBUTES (Priority of -200)     - Title, Separator, InfoBox, Required/NotDefault/Validate (InfoBox variants)
-        // WRAPPER ATTRIBUTES (Priority of -200)        - (Show/Hide/Enable/Disable)(If/In), ReadOnly, Indent/FixedIndent
-        // GROUP ATTRIBUTES (Priority of -200)          - FoldoutGroup/BoxGroup
-        // SCOPE ATTRIBUTES (Priority of -200) same as GROUP ATTRIBUTE - FoldoutScope/BoxScope, EndScope
-    }
-    
     /// <summary>
     /// Attributes that applies after normal property attributes
     /// </summary>
@@ -95,5 +77,36 @@ namespace AstrotypeInspector
             => order = 0;
     }
     
+    
+    /// <summary> Global priority of inspector attributes. Lower values are applied last. </summary>
+    public static class AttributePriority
+    {
+        public const int PostPropertyAttribute = -100;
+        public const int DecorativeAttribute = -200;
+        public const int WrapperAttribute = -200;
+        public const int GroupAttribute = -200;
+        
+        // DIFFERENT TYPES OF ATTRIBUTES : based on order and priority
+        // ----------------------------------------------------------------------
+        // UNITY PROPERTY ATTRIBUTES (Priority of 0)    - Range, Multiline, etc...
+        // POST PROPERTY ATTRIBUTES (Priority of -100)  - Slider
+        // DECORATIVE ATTRIBUTES (Priority of -200)     - Title, Separator, InfoBox, Required/NotDefault/Validate (InfoBox variants)
+        // WRAPPER ATTRIBUTES (Priority of -200)        - (Show/Hide/Enable/Disable)(If/In), ReadOnly, Indent/FixedIndent
+        // GROUP ATTRIBUTES (Priority of -200)          - FoldoutGroup/BoxGroup
+        // SCOPE ATTRIBUTES (Priority of -200) same as GROUP ATTRIBUTE - FoldoutScope/BoxScope, EndScope
+    }
+    
+    /// <summary>
+    /// List of string-exact scripting define symbols to use in Conditional attribute for attribute classes.
+    /// Made to defend against string typos.
+    /// </summary>
+    public static class Symbols
+    {
+        /// <summary> Use this to exclude attribute usages in compiled build. </summary>
+        public const string UNITY_EDITOR = nameof(UNITY_EDITOR);
+        
+        /// <summary> Use this to optionally include attribute usages in compiled build where UNITY_EDITOR is applied. </summary>
+        public const string INCLUDE_IN_BUILD = nameof(INCLUDE_IN_BUILD);
+    }
     
 }
